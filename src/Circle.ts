64faa9d4827c89418d8c5050aafed31483069210
ccccKey <ccccKey@users.noreply.github.com>
2018-01-05 00:48:34 +0800

    2018-1-5

---
+++ src/Circle.ts
@@ -0,0 +1,14 @@
+class Circle extends egret.Sprite {
+
+	public constructor(cx:number, cy:number, cr:number) {
+		super();
+		this.init(cx,cy,cr);
+	}
+
+	private shape:egret.Shape;
+
+	private init(cx:number, cy:number, cr:Number):void{
+
+	}
+
+}
